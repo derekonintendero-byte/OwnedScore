f18df4a9d5a5d3c3bea44add28b04a7177d5b26d
OwnedScore <derekonintendero@gmail.com>
2026-09-14 17:18:28 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+// firebase.js
+import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBS7zyyoSvQfllyQ8RAvdT3zU0GQGh5qUg",
   authDomain: "ownedscore.firebaseapp.com",
   projectId: "ownedscore",
   storageBucket: "ownedscore.firebasestorage.app",
-  messagingSenderId: "987656706441",
-  appId: "1:987656706441:web:98b89f831c19133a8c1118"
+  messagingSenderId: "987565706441",
+  appId: "1:987565706441:web:988b9f831c19133a8c1118"
 };
 
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
