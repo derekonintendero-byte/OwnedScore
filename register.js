04cce79580fe016c7f8b44bfb7aecda9e200488a
OwnedScore <derekonintendero@gmail.com>
2026-09-14 17:20:04 -0400

    Update register.js

--- register.js
+++ register.js
@@ -1,18 +1,44 @@
-import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+// register.js
+import { app } from "./firebase.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword
+} from "firebase/auth";
 
-const registerBtn = document.getElementById("register-btn");
-const registerEmail = document.getElementById("register-email");
-const registerPass = document.getElementById("register-pass");
+const auth = getAuth(app);
 
-registerBtn.addEventListener("click", async () => {
-  const email = registerEmail.value;
-  const pass = registerPass.value;
+const registerModal = document.getElementById("registerModal");
+const closeRegisterModal = document.getElementById("closeRegisterModal");
 
+const registerBtn = document.getElementById("registerBtn");
+const registerEmail = document.getElementById("registerEmail");
+const registerPassword = document.getElementById("registerPassword");
+
+const openRegisterFromLogin = document.getElementById("openRegisterFromLogin");
+const openLoginFromRegister = document.getElementById("openLoginFromRegister");
+const loginModal = document.getElementById("loginModal");
+
+// Abrir registro
+openRegisterFromLogin.onclick = () => {
+  loginModal.style.display = "none";
+  registerModal.style.display = "flex";
+};
+
+// Cerrar registro
+closeRegisterModal.onclick = () => registerModal.style.display = "none";
+
+// Volver a login
+openLoginFromRegister.onclick = () => {
+  registerModal.style.display = "none";
+  loginModal.style.display = "flex";
+};
+
+// Registro
+registerBtn.onclick = async () => {
   try {
-    await createUserWithEmailAndPassword(auth, email, pass);
-    window.location.href = "perfil.html";
+    await createUserWithEmailAndPassword(auth, registerEmail.value, registerPassword.value);
+    registerModal.style.display = "none";
   } catch (err) {
-    alert("Error al crear cuenta");
+    alert("Error al registrarse");
   }
-});
+};
